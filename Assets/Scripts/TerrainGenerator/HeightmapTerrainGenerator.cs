67154a4071583836accbc2ec2f17e6c378ddad95
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-06-03 18:35:50 -0300

    Na hora de criar o terreno com um tamanho maior quebrou tudo. O motivo parece ser que o algoritmo assume que a malha tem a topologia dos planes da Unity quando o plano que eu preciso criar no blender para servir de base tem provavelmente uma topologia diferente por fusão de vertices ou algo assim

--- Assets/Scripts/TerrainGenerator/HeightmapTerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator/HeightmapTerrainGenerator.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using terrainGenerator;
 using UnityEngine;
 
-public class NewBehaviourScript : ITerrainGenerator {
+public class HeightMapTerrainGenerator : ITerrainGenerator {
+	private Texture2D sourceTexture;
+	public HeightMapTerrainGenerator(Texture2D tex){
+		sourceTexture = tex;
+	}
+    
 	public  Vector2[,] GetTerrainDerivative(float[,] height)
 	{
 		throw new System.NotImplementedException();
@@ -11,7 +16,18 @@ public class NewBehaviourScript : ITerrainGenerator {
 
 	public  float[,] GetTerrainHeight(int width, float scale)
 	{
-		throw new System.NotImplementedException();
+		float[,] map = new float[width, width];
+		Color[] pixelData = sourceTexture.GetPixels();
+		for (int zIndex = 0; zIndex < width; zIndex++)
+		{
+			for (int xIndex = 0; xIndex < width; xIndex++)
+			{
+				//float heightValueInTexture = pixelData[zIndex * width + xIndex].r;
+				float heightValueInTexture = sourceTexture.GetPixel(xIndex, zIndex).r;
+				map[zIndex, xIndex] = heightValueInTexture;
+			}
+		}
+		return map;
 	}
 
 }
---
+++ Assets/Scripts/terain/MyTerrain.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using terrainGenerator;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MyTerrain : MonoBehaviour {
+	public Texture2D HeightmapTexture;
+	/// <summary>
+    /// Um multiplicador que é a aplicado à altitude.
+    /// </summary>
+	public float HeightMultiplier;
+	/// <summary>
+    /// O gerador do terreno.
+    /// </summary>
+	private ITerrainGenerator generator;
+	// Use this for initialization
+	void Start () {
+		Assert.IsNotNull(HeightmapTexture, "É obrigatório informar a textura do heightmap");
+		Assert.IsFalse(HeightMultiplier == 0, "HeightMultiplier não pode ser zero.");
+		Assert.IsNotNull(GetComponent<MeshCollider>(), "Tem que ter mesh collider");
+		//generator = new HeightMapTerrainGenerator(HeightmapTexture);      
+		generator = new RandomTerrainGenerator();
+		int width = CalculateWidth();
+		float[,] heightData = generator.GetTerrainHeight(width, 10);
+		SetTerrainHeight(heightData);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void SetTerrainHeight(float[,] height){
+		int tileDepth = height.GetLength(0);
+		int tileWidth = height.GetLength(1);
+		Vector3[] meshVertices = GetComponent<MeshFilter>().mesh.vertices;
+		int vertexIndex = 0;
+		for (int zIndex = 0; zIndex < tileDepth; zIndex++)
+		{
+			for (int xIndex = 0; xIndex < tileWidth; xIndex++)
+			{
+				float currHeight = height[zIndex, xIndex];
+				Vector3 vert = meshVertices[vertexIndex];
+				meshVertices[vertexIndex] = new Vector3(vert.x, currHeight * HeightMultiplier, vert.z);
+				vertexIndex++;
+			}
+		}
+		// update the vertices in the mesh and update its properties
+		GetComponent<MeshFilter>().mesh.vertices = meshVertices;
+		GetComponent<MeshFilter>().mesh.RecalculateBounds();
+		GetComponent<MeshFilter>().mesh.RecalculateNormals();
+		GetComponent<MeshFilter>().mesh.RecalculateTangents();
+		// update the mesh collider
+		GetComponent<MeshCollider>().sharedMesh = GetComponent<MeshFilter>().mesh;
+	}
+    /// <summary>
+    /// Meu objeto é um plano. Planos da unity são quadrados.
+    /// </summary>
+    /// <returns>The width.</returns>
+	private int CalculateWidth(){
+		Vector3[] vertices = GetComponent<MeshFilter>().mesh.vertices;
+		return (int)Mathf.Sqrt(vertices.Length);
+	}
+
+}
