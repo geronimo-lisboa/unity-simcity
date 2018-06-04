d9f2a59e76b4b160597b50fccbc06d685d4e7b54
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-06-04 20:43:41 -0300

    O gerador que gera um plano de tamanho arbitrário está começando a ser feito. Ele já gera esse plano em tempo de execução.

--- Assets/Scripts/planeCreator/PlaneSource.cs
+++ Assets/Scripts/planeCreator/PlaneSource.cs
@@ -1,65 +1,62 @@
-﻿using System.Collections;
+﻿////https://catlikecoding.com/unity/tutorials/procedural-grid/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class PlaneSource : MonoBehaviour {
-	private int currentWidth = 0;
-	public int Width = 2;
-
-	public PlaneSource(){
+    public int xSize, ySize;
+    private Mesh mesh;
+    private Vector3[] vertices;
+	// Use this for initialization
+	void Start () {
 		
 	}
 
-	void Awake()
-	{
-        
+    private void Awake()
+    {
+        Generate();
     }
-
-    void Update()
-	{
-		//Debug.Log("Width=" + Width + ", currentWidth=" + currentWidth);
-		if(currentWidth!=Width)//Mexeu na width, reconstruir a geometria.
-		{
-			Debug.Log("alteracao");
-			BuildPlane();
-			currentWidth = Width;         
-		}
-    }
-
-	private void BuildPlane(){
-		Mesh mesh = new Mesh();
-		mesh.name = "scripted plane";
-
-		List<Vector3> vertexData = new List<Vector3>();
-        vertexData.Add(new Vector3(-1, -1, 0));
-		vertexData.Add(new Vector3( 1, -1, 0));
-		vertexData.Add(new Vector3( 1,  1, 0));
-		vertexData.Add(new Vector3(-1,  1, 0));
-
-		List<Vector2> uvs = new List<Vector2>();
-		uvs.Add(new Vector2(0, 0));
-		uvs.Add(new Vector2(0, 1));
-		uvs.Add(new Vector2(1, 1));
-		uvs.Add(new Vector2(1, 0));
-
-		List<int> triangles = new List<int>();
-		triangles.Add(0);
-		triangles.Add(1);
-		triangles.Add(2);
-		triangles.Add(0);
-		triangles.Add(2);
-		triangles.Add(3);
-
-		mesh.vertices = vertexData.ToArray();
-		mesh.uv = uvs.ToArray();
-		mesh.triangles = triangles.ToArray();
-		mesh.RecalculateBounds();
-		mesh.RecalculateNormals();
-		mesh.RecalculateTangents();
-
-		GetComponent<MeshFilter>().mesh = mesh;
-		GetComponent<MeshCollider>().sharedMesh = mesh; 
-
+    // Update is called once per frame
+    void Update () {
+		
 	}
+
+    private void Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+        for (int i = 0, y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+        }
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+    }
+
+    
 }
