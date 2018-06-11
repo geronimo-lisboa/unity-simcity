3194e30374e5db45c8236eb1dd8983b32eb9d4aa
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-06-11 07:03:59 -0300

    Dificuldades em fazer o modelo de dominio do editor.

---
+++ Assets/Modificador.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class TipoDeModificacao
+{
+    public bool Incremento { get; private set; }
+    public TipoDeModificacao(bool incremento)
+    {
+        this.Incremento = incremento;
+    }
+}
+
+class ModificacaoLinear : TipoDeModificacao
+{
+    public ModificacaoLinear(bool inc):base(inc)
+    {
+;
+    }
+}
+
+public class Modificador : MonoBehaviour {
+    //Os botões
+    public Button ButtonRaiseTerrain;
+    public Button ButtonLowerTerrain;
+
+    private TipoDeModificacao currentTypeOfModification = null;
+	// Use this for initialization
+	void Start () {
+        //Seta os delegates que vão tratar dos clicks
+        ButtonLowerTerrain.onClick.AddListener(delegate
+        {
+            currentTypeOfModification = new ModificacaoLinear(false);
+        });
+        ButtonRaiseTerrain.onClick.AddListener(delegate
+        {
+            currentTypeOfModification = new ModificacaoLinear(true);
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
