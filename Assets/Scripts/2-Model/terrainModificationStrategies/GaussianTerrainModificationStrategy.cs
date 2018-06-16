d57155db52a5c0244c86854ff580db77145a2938
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-06-16 16:26:33 -0300

    Sofrendo com a implementação do gaussiano.

--- Assets/Scripts/2-Model/terrainModificationStrategies/GaussianTerrainModificationStrategy.cs
+++ Assets/Scripts/2-Model/terrainModificationStrategies/GaussianTerrainModificationStrategy.cs
@@ -5,13 +5,44 @@ namespace model.terrain.terrainModificationStrategy
 {
     public class GaussianTerrainModificationStrategy : TerrainModificationStrategy
     {
+        
+
         public GaussianTerrainModificationStrategy(ElevationChange raiseOrLower) : base(raiseOrLower)
         {
         }
+        
+        private float EvaluateGaussian(int[] center, int[] position, float σx = 1, float σy = 1, float A = 1)
+        {
+            //Implementação BEEEM mastigada do gaussiano, focada em clareza e não velocidade
+            float dX = Mathf.Pow(position[0] - center[0], 2); //o (x-x0)^2 da equação
+            float dY = Mathf.Pow(position[1] - center[1], 2); //o (y-y0)^2 da equação
 
+            float XdividedBySigma = dX / (2 * Mathf.Pow(σx, 2));
+            float YdividedBySigma = dY / (2 * Mathf.Pow(σy, 2));
+
+            float XPlusY = -1 * (XdividedBySigma + YdividedBySigma);
+
+            float H = A * Mathf.Exp(XPlusY);
+
+            return H;
+        }
+//        https://en.wikipedia.org/wiki/Gaussian_function
+//        Olhar a sessão de gaussiano 2d
         public override void Execute(MyTerrain terrain, float intensity, Vector3 pointInWC)
         {
-            throw new System.NotImplementedException();
+            int[] pointInImageCoordinate = WorldCoordinateToImageCoordinate(terrain, pointInWC);
+            //pointInImageCoordinate é o centro da função gaussiana
+            float[,] gaussianMask = new float[terrain.Heightmap.width, terrain.Heightmap.height];//Onde vou guardar os valores do gaussiano. 
+            //Esses valores serão somados aos valores da imagem
+            for (int i = 0, z = 0; z <= terrain.Heightmap.height; z++)
+            {
+                for (int x = 0; x <= terrain.Heightmap.width; x++, i++)
+                {
+
+                }
+            }
+            
+
         }
     }
 }
