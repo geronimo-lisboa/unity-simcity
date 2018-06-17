d35e28bb60092e45a53b7e62d0af6d5f178414ba
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-06-17 14:32:39 -0300

    Gaussiano está cheio de problemas mas pela 1a vez deformou o terreno.

--- Assets/Scripts/2-Model/terrainModificationStrategies/GaussianTerrainModificationStrategy.cs
+++ Assets/Scripts/2-Model/terrainModificationStrategies/GaussianTerrainModificationStrategy.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 namespace model.terrain.terrainModificationStrategy
 {
+
+
     public class GaussianTerrainModificationStrategy : TerrainModificationStrategy
     {
+        Texture2D CreateTexture(float [,] mat)
+        {
+            int xLen = (int)Mathf.Sqrt(mat.Length);
+            var texture = new Texture2D(xLen, xLen, TextureFormat.ARGB32, false);
+            for(var z=0; z<xLen; z++)
+            {
+                for(var x=0; x< xLen; x++)
+                {
+                    Color c = new Color(mat[z, x], mat[z, x], mat[z, x], mat[z, x]);
+                    texture.SetPixel(z, x, c);
+                }
+            }
+            texture.Apply();
+            return texture;
+        }
+
+        void SaveTextureToFile(Texture2D texture, string fileName)
+        {
+            var bytes = texture.EncodeToPNG();
+            var file = File.Open(Application.dataPath + "/" + fileName, FileMode.Create);
+            var binary = new BinaryWriter(file);
+            binary.Write(bytes);
+            file.Close();
+        }
 
 
         public GaussianTerrainModificationStrategy(ElevationChange raiseOrLower) : base(raiseOrLower)
@@ -27,6 +54,7 @@ namespace model.terrain.terrainModificationStrategy
 
             return H;
         }
+        int fileCount = 0;
         //        https://en.wikipedia.org/wiki/Gaussian_function
         //        Olhar a sessão de gaussiano 2d
         public override void Execute(MyTerrain terrain, float intensity, Vector3 pointInWC)
@@ -36,16 +64,17 @@ namespace model.terrain.terrainModificationStrategy
             //pointInImageCoordinate é o centro da função gaussiana
             float[,] gaussianMask = new float[terrain.Heightmap.width, terrain.Heightmap.height];//Onde vou guardar os valores do gaussiano. 
             //Esses valores serão somados aos valores da imagem
-            for (int i = 0, z = 0; z <= terrain.Heightmap.height; z++)
+            for (int i = 0, z = 0; z < terrain.Heightmap.height; z++)
             {
-                for (int x = 0; x <= terrain.Heightmap.width; x++, i++)
+                for (int x = 0; x < terrain.Heightmap.width; x++, i++)
                 {
                     //TODO: Usar o gaussiano
                     int[] p = new int[] { x, z };
-                    float v = EvaluateGaussian(pointInImageCoordinate, p, 200, 200, 200);
-                    //gaussianMask[z, x] = v;
+                    float v = EvaluateGaussian(pointInImageCoordinate, p, intensity/2, intensity*2, intensity*2);
+                    gaussianMask[z, x] = v;
                 }
             }
+            SaveTextureToFile(CreateTexture(gaussianMask), "gaussian"+(++fileCount)+".png");
             Texture2D hm = terrain.Heightmap;
             //TODO: Somar a imagem do guassiano à imagem do heightmap
             for (int i = 0, z = 0; z < terrain.Heightmap.height; z++)
